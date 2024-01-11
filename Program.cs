1734f8eb0840d20c4470250d207af354857de158
A252420 <A252420@LSBSA-GQV6HW3>
2024-01-11 12:21:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30.Pascal_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows you wish to see in pascal triangle:");
+
+            //Freeze console
+            int var = int.Parse(Console.ReadLine());
+            int rows = var, val = 1;
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int blank = 1; blank <= rows - i; blank++)
+                    Console.Write(" ");
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        val = 1;
+                    else
+                        val = val * (i - j + 1) / j;
+                    Console.Write(val + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
